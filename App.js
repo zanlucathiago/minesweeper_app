26599b8333998d79d7097405da482999985615aa
Thiago Monteiro Zanluca <zanlucathiago@gmail.com>
2020-06-02 22:48:42 -0300

    Versoes latest Bump 1.1.0 Adicionados arquivos do bundle Nome do app e icones Versão offline finalizada.

--- App.js
+++ App.js
@@ -1,9 +1,43 @@
+// import NetInfo from '@react-native-community/netinfo';
 import React, {Component} from 'react';
 import {WebView} from 'react-native-webview';
 
+// import {Image} from 'react-native';
+// import {ActivityIndicator, View} from 'react-native';
+// const index = require('./build/index.html');
+// const source = require('./build/index.html');
+// const webviewSource = Image.resolveAssetSource(source);
 class MyWeb extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {};
+
+  //   NetInfo.fetch().then((state) => {
+  //     console.log('Connection type', state.type);
+  //     console.log('Is connected?', state.isConnected);
+
+  //     this.setState({
+  //       type: state.type,
+  //     });
+  //   });
+  // }
+
   render() {
-    return <WebView source={{uri: 'https://minesweeper.virtualab.tech'}} />;
+    // const {type} = this.state;
+
+    // if (!type) {
+    //   return null;
+    // }
+
+    // const low = type !== 'wifi';
+    return <WebView source={{uri: 'file:///android_asset/index.html'}} />;
+    // return (
+    //   <WebView
+    //     source={require('./android/app/src/main/assets/index.html')}
+    //     startInLoadingState
+    //   />
+    //   // <WebView url="./android/app/src/main/assets/index.html" />
+    // );
   }
 }
 
